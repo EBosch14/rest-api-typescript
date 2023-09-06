dddbb9ae60a634e334f90e0299c8d8ab501cc902
EBosch14 <enzo.jbosch@gmail.com>
2023-09-06 07:15:18 -0300

    refactor: :wrench: change config files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import { config } from "./config";
 
-function bootstrap(): void {
+function bootstrap() {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(morgan("dev"));
-  const { SV_PORT } = process.env ?? 3333;
-  app.listen(SV_PORT, () => {
-    console.log(`Server running on port ${SV_PORT}`);
+  const { port } = config.server;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 }
 
